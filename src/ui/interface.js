95f6fcd21f0bafec6e4a0382d9ff9153b6f33600
Neeraj Ramesh <neerajr@LAPTOP-0HDHF74P.localdomain>
2022-12-12 19:14:39 -0500

    added audioState and modified A hotkey

--- src/ui/interface.js
+++ src/ui/interface.js
@@ -49,6 +49,7 @@ export class UI {
 		this.$activebox = $j('#activebox');
 		this.$scoreboard = $j('#scoreboard');
 		this.active = false;
+		this.audioState = 'full audio';
 
 		// Last clicked creature in Godlet Printer for the current turn
 		this.lastViewedCreature = '';
@@ -1224,6 +1225,21 @@ export class UI {
 				this.lastViewedCreature = creatureType;
 				this.showCreature(creatureType, this.selectedPlayer, 'grid');
 			});
+	}
+
+	changeAudio() {
+		if (this.audioState = 'full audio'){
+			this.audioState = 'sfx';
+			this.game.soundsys.playMusic();
+			this.game.soundsys.setEffectsVolume(this.value);
+		} else if (this.audioState = 'sfx') {
+			this.audioState = 'no audio';
+			this.game.soundsys.stopMusic();
+			this.game.soundsys.setEffectsVolume(this.value);
+		} else if (this.audioState = 'no audio')
+			this.audioState = 'full audio';
+			this.game.soundsys.stopMusic();
+			this.game.soundsys.setEffectsVolume(0);
 	}
 
 	toggleMusicPlayer() {
